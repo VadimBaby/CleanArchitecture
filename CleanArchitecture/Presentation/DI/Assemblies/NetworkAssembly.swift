4ee70d2d2593b434497af5eb2aa865c41ae1286f
VadimBaby <vadimdim22@gmail.com>
2025-05-24 15:15:33 +0700

    fix bug

--- CleanArchitecture/Presentation/DI/Assemblies/NetworkAssembly.swift
+++ CleanArchitecture/Presentation/DI/Assemblies/NetworkAssembly.swift
@@ -12,7 +12,7 @@ class NetworkAssembly: Assembly {
     func assemble(container: Container) {
         container.register(Network.self) { _ in
             BaseNetworkService()
-        }
+        }.inObjectScope(.transient) // resolver.resolve(Network.self) всегда будет возвращать новый экземпляр класса
         
         container.register(NetworkAPI.self) { resolver in
             NetworkAPIService(network: resolver.resolve(Network.self)!)
